a503d31c71d648454aa5727bd1225976728feecb
Mohammad Mousavi <38520491+MBmousavi@users.noreply.github.com>
2021-03-08 10:04:51 +0330

    Update main.go

--- Docker/main.go
+++ Docker/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+        "fmt"
+        "net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello!")
-	})
+        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+                fmt.Fprintf(w, "hello!")
+        })
 
-	err := http.ListenAndServe(":8020", nil)
-	if err == nil {
-		panic(err)
-	}
+        http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+                w.Header().Set("content-type", "application/json")
+                fmt.Fprintf(w, `{"status":"UP"}`)
+        })
+
+        err := http.ListenAndServe(":8020", nil)
+        if err == nil {
+                panic(err)
+        }
 }
